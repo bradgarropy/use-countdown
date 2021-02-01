a744904e62c5e942c16b10d23af8aaee9a6e2c08
Brad Garropy <bradgarropy@gmail.com>
2021-01-31 21:54:34 -0600

    update file structure.

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,5 @@
-import {useEffect, useRef, useState} from "react"
-
-import {
-    calculateInitialTime,
-    calculateRemainingMinutes,
-    calculateRemainingSeconds,
-} from "./time"
+import useCountdown from "./countdown"
 import {Time} from "./types"
-
-type useCountdownParams = {
-    minutes?: number
-    seconds?: number
-}
-
-/**
- * @name useCountdown
- * @description React hook countdown timer.
- */
-const useCountdown = ({minutes = 0, seconds = 0}: useCountdownParams): Time => {
-    const id = useRef(0)
-
-    const [remainingTime, setRemainingTime] = useState(
-        calculateInitialTime({minutes, seconds}),
-    )
-
-    useEffect(() => {
-        id.current = window.setInterval(calculateRemainingTime, 1000)
-        return () => window.clearInterval(id.current)
-    }, [])
-
-    const calculateRemainingTime = () => {
-        setRemainingTime(time => {
-            if (time - 1000 <= 0) {
-                clearInterval(id.current)
-                return 0
-            }
-
-            return time - 1000
-        })
-    }
-
-    const countdown = {
-        minutes: calculateRemainingMinutes(remainingTime),
-        seconds: calculateRemainingSeconds(remainingTime),
-    }
-
-    return countdown
-}
 
 export default useCountdown
 export {Time}
