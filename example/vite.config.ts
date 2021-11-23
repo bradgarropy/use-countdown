e4a44ac3044b9198e6db622b63a4f2674a62a1da
Brad Garropy <bradgarropy@gmail.com>
2021-11-23 13:30:22 -0600

    remove comments.

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,9 +2,5 @@ import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
 
 export default defineConfig({
-    // esbuild: {
-    // eslint-disable-next-line quotes
-    // jsxInject: 'import React from "react"',
-    // },
     plugins: [react()],
 })
